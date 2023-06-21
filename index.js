21e7e444bec6642b0ff1bdcc947679215ef54b6c
cyrille <cyrilllerotich@gmail.com>
2023-06-22 02:57:08 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (num1, num2) => {
+    num1 / num2;
+    return 2000 / 100;
+}
+const square = (number) => {
+    number * number
+    return 2 * 2
+
+}
+const add = (a, b) => {
+    a + b
+    return 3 + 4
+
+}
